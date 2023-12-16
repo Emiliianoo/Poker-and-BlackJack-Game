774e342ca391e6b66ada3d1f74ae7a4f0ab15021
DonPedroloDev <31079337@modelo.edu.mx>
2023-12-15 22:27:54 -0600

    Player Functions Created
    The main structure of JugadorPoker and the behaviors of recive, return, and show cards were created

--- Models/JugadorPoker.cs
+++ Models/JugadorPoker.cs
@@ -5,29 +5,135 @@ namespace CardGame.Models
 {
     public class JugadorPoker : IJugador
     {
+       //Atributos del jugador
+        private string _nombre;        
+        public string Nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+            set
+            {
+                if(string.IsNullOrWhiteSpace(value))
+                {
+                    throw new NotImplementedException();
+                }
+                _nombre = value;
+            }
+        }
+
+        private List<ICarta> _cartas;
+        public List<ICarta> Cartas
+        {
+            get
+            {
+                return _cartas;
+            }
+            set
+            {
+                if(value == null)
+                {
+                    throw new NotImplementedException();
+                }
+                _cartas = value;
+            }
+        }
+
+        
+
+        //Constructor del jugador
+        public JugadorPoker(string nombre = "Jugador")
+        {
+            Nombre = nombre;
+            Cartas = new List<ICarta>();
+        }
+
         public ICarta DevolverCarta(int indiceCarta)
         {
-            throw new NotImplementedException();
+            //Devuelve la carta que se encuentra en el indice indicado si es que este existe
+            if (indiceCarta < Cartas.Count)
+            {
+                //Obtener la carta que se encuentra en el indice indicado            
+                ICarta cartaADevolver = Cartas[indiceCarta];         
+
+                //Quitar la carta de la lista de cartas del jugador
+                Cartas.RemoveAt(indiceCarta);
+
+                //Devolver la carta
+                return cartaADevolver;
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
         }
 
         public List<ICarta> DevolverTodasLasCartas()
         {
-            throw new NotImplementedException();
+            //Devuelve todas las cartas del jugador si es que este tiene cartas
+            if (Cartas.Count == 0)
+            {
+                throw new NotImplementedException();
+            }
+            else
+            {
+                //obtener todas las cartas del jugador
+                List<ICarta> cartasADevolver = Cartas;
+
+                //Quitar todas las cartas de la lista de cartas del jugador
+                Cartas.Clear();
+
+                //Devolver todas las cartas
+                return cartasADevolver;
+            }
         }
 
         public ICarta MostrarCarta(int indiceCarta)
         {
-            throw new NotImplementedException();
+            //Muestra la carta que se encuentra en el indice indicado si es que este existe
+            if (indiceCarta < Cartas.Count)
+            {
+                //Mostrar la carta en la consola
+                Console.WriteLine(Cartas[indiceCarta].Figura + " de " + Cartas[indiceCarta].Valor);
+                return Cartas[indiceCarta];
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
         }
 
         public List<ICarta> MostrarCartas()
         {
-            throw new NotImplementedException();
+             //Muestra todas las cartas del jugador si es que este tiene cartas
+            if (Cartas.Count == 0)
+            {
+                throw new NotImplementedException();
+            }
+            else
+            {
+                //Mostrar todas las cartas en la consola
+                foreach (ICarta carta in Cartas)
+                {
+                    Console.WriteLine(carta.Figura + " de " + carta.Valor);
+                }
+                return Cartas;
+            }
+
+
         }
 
         public void ObtenerCartas(List<ICarta> cartas)
         {
-            throw new NotImplementedException();
+            
+            //Si las cartas recibidas no estan vacias se agregan a la lista de cartas del jugador
+            if (cartas.Count != 0) {
+                Cartas.AddRange(cartas);
+            } else {
+                throw new NotImplementedException();
+            }
+
         }
 
         public void RealizarJugada()
