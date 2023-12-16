1763082c5b92b28c585f81cd88526295292146ee
DonPedroloDev <31079337@modelo.edu.mx>
2023-12-16 17:21:20 -0600

    Now theres a JugadorBlackJack class now
    The file of JugadorBlackJackClass was created and the intarface was applied.

---
+++ Models/JugadorBlackJack.cs
@@ -0,0 +1,39 @@
+using System;
+using CardGame.Interfaces;
+
+namespace CardGame.Models
+{
+    public class JugadorBlackJack : IJugador
+    {
+        public ICarta DevolverCarta(int indiceCarta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ICarta> DevolverTodasLasCartas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICarta MostrarCarta(int indiceCarta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ICarta> MostrarCartas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ObtenerCartas(List<ICarta> cartas)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RealizarJugada()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
